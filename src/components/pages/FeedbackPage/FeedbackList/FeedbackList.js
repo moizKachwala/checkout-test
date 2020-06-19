a7a44656c28b0209908c23d42b785c6db25fffde
Moiz Kachwala <moizk@hotmail.com>
2020-06-19 22:34:23 +0400

    reversed the feedbacks

--- src/components/pages/FeedbackPage/FeedbackList/FeedbackList.js
+++ src/components/pages/FeedbackPage/FeedbackList/FeedbackList.js
@@ -35,7 +35,7 @@ export default function FeedbackList({ feedbacks = [] }) {
                         </Typography>
                     </CardContent>
                 </Card>
-            ))}
+            )).reverse()}
         </Paper>
     )
 }
