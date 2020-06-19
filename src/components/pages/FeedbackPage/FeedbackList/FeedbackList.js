425dec8fa340e66d708ac3c059b14055b2ef10e0
Moiz Kachwala <moizk@hotmail.com>
2020-06-19 20:02:34 +0400

    added margin

--- src/components/pages/FeedbackPage/FeedbackList/FeedbackList.js
+++ src/components/pages/FeedbackPage/FeedbackList/FeedbackList.js
@@ -4,6 +4,9 @@ import { Rating } from '@material-ui/lab';
 
 const useStyles = makeStyles(theme => ({
     root: {
+        marginTop: theme.spacing(2),
+    },
+    card: {
         marginTop: theme.spacing(0.5),
     },
 }));
@@ -11,12 +14,12 @@ const useStyles = makeStyles(theme => ({
 export default function FeedbackList({ feedbacks = [] }) {
     const classes = useStyles();
     return (
-        <Paper>
+        <Paper className={classes.root}>
             <Typography align="center" gutterBottom>
                 {feedbacks.length > 0 ? "All Feedbacks" : "No feedbacks added yet!"}
             </Typography>
             {feedbacks.map(({ name, email, rating, comment }, index) => (
-                <Card className={classes.root} key={index} variant="outlined" square>
+                <Card className={classes.card} key={index} variant="outlined" square>
                     <CardHeader
                         title={name}
                         subheader={email}
