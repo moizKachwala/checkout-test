98648bbf2cec37a62f2ae35739361b32b5896057
Moiz Kachwala <moizk@hotmail.com>
2020-06-19 13:42:35 +0400

    added rating component

--- src/components/pages/FeedbackPage/FeedbackList.js
+++ src/components/pages/FeedbackPage/FeedbackList.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { Card, CardContent, Typography, CardHeader } from '@material-ui/core';
+import { Rating } from '@material-ui/lab';
 
-export default function FeedbackList({feedbacks = []}) {
+export default function FeedbackList({ feedbacks = [] }) {
     return (
         <div className="feedback-list">
-            {feedbacks.map(({name, email, rating, comment}, index) => (
-                <div key={index}>
-                    {name}
-                    {email}
-                    {rating}
-                    {comment}
-                </div>
-            ))}
+            <Card>
+                {feedbacks.map(({ name, email, rating, comment }, index) => (
+                    <div key={index}>
+                        <CardHeader
+                            title={name}
+                            subheader={email}
+                            action={
+                                <Rating readOnly value={rating} />
+                            }
+                        />
+                        <CardContent>
+                            <Typography paragraph>
+                                {comment}
+                            </Typography>
+                        </CardContent>
+                    </div>
+                ))}
+            </Card>
         </div>
     )
 }
--- src/components/pages/FeedbackPage/FeedbackPage.js
+++ src/components/pages/FeedbackPage/FeedbackPage.js
@@ -3,6 +3,10 @@ import { Formik } from 'formik';
 import {
     Button,
     TextField,
+    Grid,
+    Card,
+    CardContent,
+    CardActions,
 } from '@material-ui/core';
 import { Rating } from '@material-ui/lab';
 
@@ -22,91 +26,105 @@ export default function FeedbackPage(props) {
     }
 
     return (
-        <>
-            <Formik
-                initialValues={
-                    {
-                        name: '',
-                        email: '',
-                        rating: 0,
-                    }}
-                onSubmit={(values, actions) => {
-                    handleSumit(values);
-                }}
-            >
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                }) => (
-                        <form
-                            noValidate={true}
-                            autoComplete="off"
-                            onSubmit={handleSubmit}
-                        >
-                            <div className="input-group">
-                                <TextField
-                                    type="name"
-                                    label="Name"
-                                    name="name"
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    value={values.name}
-                                    helperText={touched.name && errors.name}
-                                    error={Boolean(touched.name && errors.name)}
-                                />
-                            </div>
-                            <div className="input-group">
-                                <TextField
-                                    type="email"
-                                    label="Email"
-                                    name="email"
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    value={values.email}
-                                    helperText={touched.email && errors.email}
-                                    error={Boolean(touched.email && errors.email)}
-                                />
-                            </div>
-                            <div className="input-group">
-                                <Rating
-                                    name="rating"
-                                    value={values.rating}
-                                    onChange={handleChange}
-                                    size="large" />
-                            </div>
-                            <div className="input-group">
-                                <TextField
-                                    type="comment"
-                                    label="Comment"
-                                    multiline
-                                    rows="4"
-                                    name="comment"
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    value={values.comment}
-                                    helperText={touched.comment && errors.comment}
-                                    error={Boolean(touched.comment && errors.comment)}
-                                />
-                            </div>
-                            <div className="">
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    type="submit"
-                                    size="large"
-                                >
-                                    Submit
-                            </Button>
-                            </div>
-                        </form>
-                    )}
-            </Formik>
+        <div className="feedback-page">
+            <Grid container spacing={1}>
+                <Grid item xs={12} sm={12} md={6}>
+                    <Formik
+                        initialValues={
+                            {
+                                name: 'Moiz Kachwala',
+                                email: 'moizk@hotmail.com',
+                                rating: 4,
+                                comment: 'This is a test comment for you to go through ti and the product is very good and useful'
+                            }}
+                        onSubmit={(values, actions) => {
+                            handleSumit(values);
+                        }}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                        }) => (
 
-            <FeedbackList feedbacks={feedbacks} />
-        </>
+                                <form
+                                    noValidate={true}
+                                    autoComplete="off"
+                                    onSubmit={handleSubmit}
+                                >
+                                    <Card>
+                                        <CardContent>
+                                            <div className="input-group">
+                                                <TextField
+                                                    type="name"
+                                                    label="Name"
+                                                    name="name"
+                                                    onBlur={handleBlur}
+                                                    onChange={handleChange}
+                                                    value={values.name}
+                                                    helperText={touched.name && errors.name}
+                                                    error={Boolean(touched.name && errors.name)}
+                                                />
+                                            </div>
+                                            <div className="input-group">
+                                                <TextField
+                                                    type="email"
+                                                    label="Email"
+                                                    name="email"
+                                                    onBlur={handleBlur}
+                                                    onChange={handleChange}
+                                                    value={values.email}
+                                                    helperText={touched.email && errors.email}
+                                                    error={Boolean(touched.email && errors.email)}
+                                                />
+                                            </div>
+                                            <div className="input-group">
+                                                <Rating
+                                                    name="rating"
+                                                    value={values.rating}
+                                                    onChange={handleChange}
+                                                    size="large" />
+                                            </div>
+                                            <div className="input-group">
+                                                <TextField
+                                                    type="comment"
+                                                    label="Comment"
+                                                    multiline
+                                                    rows="4"
+                                                    name="comment"
+                                                    onBlur={handleBlur}
+                                                    onChange={handleChange}
+                                                    value={values.comment}
+                                                    helperText={touched.comment && errors.comment}
+                                                    error={Boolean(touched.comment && errors.comment)}
+                                                />
+                                            </div>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button
+                                                variant="contained"
+                                                color="primary"
+                                                type="submit"
+                                                size="large"
+                                            >
+                                                Submit
+                                        </Button>
+                                        </CardActions>
+                                    </Card>
+                                </form>
+                            )}
+                    </Formik>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6}>
+                    Graph
+                </Grid>
+                <Grid item xs={12} sm={12} md={12}>
+                    <FeedbackList feedbacks={feedbacks} />
+                </Grid>
+            </Grid>
+        </div>
     );
 }
