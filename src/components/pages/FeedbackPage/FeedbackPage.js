538cb4d6935d0ce6d34ed85a6fa8f7778e1bb588
Moiz Kachwala <moizk@hotmail.com>
2020-06-19 10:45:33 +0400

    added feedback form

--- src/components/pages/FeedbackPage/FeedbackPage.js
+++ src/components/pages/FeedbackPage/FeedbackPage.js
@@ -1,33 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik } from 'formik';
+import {
+    Button,
+    TextField,
+} from '@material-ui/core';
+import {Rating} from '@material-ui/lab';
 
 export default function FeedbackPage(props) {
 
-    const handleSumit = (payload) => {
-        console.log(payload);
+    const [feedbacks, setFeedbacks] = useState([]);
+
+    const handleSumit = (feedback) => {
+        console.log(feedback);
+        setFeedbacks([feedback]);
     }
 
     return (
         <>
             <Formik
-                initialValues={{ name: 'jared' }}
+                initialValues={
+                    {
+                        name: '',
+                        email: '',
+                        rating: 0,
+                    }}
                 onSubmit={(values, actions) => {
-                    handleSumit(values);
+                    console.log(values);
                 }}
             >
-                {props => (
-                    <form onSubmit={props.handleSubmit}>
-                        <input
-                            type="text"
-                            onChange={props.handleChange}
-                            onBlur={props.handleBlur}
-                            value={props.values.name}
-                            name="name"
-                        />
-                        {props.errors.name && <div id="feedback">{props.errors.name}</div>}
-                        <button type="submit">Submit</button>
-                    </form>
-                )}
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                }) => (
+                        <form 
+                            noValidate={true}
+                            autoComplete="off"
+                            onSubmit={handleSubmit}
+                            >
+                            <div className="input-group">
+                                <TextField
+                                    type="name"
+                                    label="Name"
+                                    margin="normal"
+                                    name="name"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.name}
+                                    helperText={touched.name && errors.name}
+                                    error={Boolean(touched.name && errors.name)}
+                                />
+                            </div>
+                            <div className="input-group">
+                                <TextField
+                                    type="email"
+                                    label="Email"
+                                    margin="normal"
+                                    name="email"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.email}
+                                    helperText={touched.email && errors.email}
+                                    error={Boolean(touched.email && errors.email)}
+                                />
+                            </div>
+                            <div className="input-group">
+                                <Rating
+                                    name="rating"
+                                    value={values.rating}
+                                    onChange={handleChange}
+                                    size="large" />
+                            </div>
+                            <div className="">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    type="submit"
+                                    size="large"
+                                >
+                                    Submit
+                            </Button>
+                            </div>
+                        </form>
+                    )}
             </Formik>
         </>
     );
