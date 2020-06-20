1a4efded1f500c2d011e56a585bae8fca81224f9
Moiz Kachwala <moizk@hotmail.com>
2020-06-20 15:19:30 +0400

    fixed form validations

--- src/components/pages/FeedbackPage/FeedbackValidation/validation.js
+++ src/components/pages/FeedbackPage/FeedbackValidation/validation.js
@@ -3,6 +3,19 @@ export const EMAIL_REGEX = /^[_A-Za-z0-9-+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.
 export const validate = values => {
     const errors = {};
 
+    if (!values.name) {
+        errors.name = "Name is required"
+    }
+
+    if (!values.email) {
+        errors.email = "Email is required"
+    } else if (!values.email.match(EMAIL_REGEX)) {
+        errors.email = "Email is invalid"
+    }
+
+    if (!values.rating) {
+        errors.rating = "Rating is required"
+    }
 
     return errors;
 };
